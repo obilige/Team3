b8e48be6f08ed145bb980c0610631b793f2950c0
obilige <jaehoon-@hotmail.com>
2022-07-21 23:37:37 +0900

    DL update

--- DL_module/utils.py
+++ DL_module/utils.py
@@ -16,3 +16,6 @@ def trans_timeseries(feature_df, label_df, time_step):
         feature_list.append(feature_df[i:i+time_step])
         label_list.append(label_df.iloc[i+time_step])
     return np.array(feature_list), np.array(label_list)
+
+def extra():
+    pass
